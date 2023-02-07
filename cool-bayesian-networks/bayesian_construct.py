65d9ec9bee1acfc2cf0d36c4b109b97b3f15f3a5
unknown <staracenapuga@gmail.com>
2023-02-07 16:33:50 -0600

    Code refactoring finished

---
+++ cool-bayesian-networks/bayesian_construct.py
@@ -0,0 +1,54 @@
+from pgmpy.models import BayesianNetwork
+from pgmpy.factors.discrete import TabularCPD 
+from pgmpy.inference import VariableElimination
+
+class Bayesian(object):
+
+    def __init__(self):
+        self.model = None
+        self.infer = None
+
+    # Creación del modelo bayesiano
+    def bayesian_network_construction(self, construct):
+        self.model = BayesianNetwork(construct)
+
+    # Definición de las probabilidades condicionales
+    def asign_values(self, name, amount, probabilities, evidences=None, evidences_card=None):
+        return TabularCPD(
+            variable=name,
+            variable_card=amount, 
+            values=probabilities,
+            evidence=evidences,
+            evidence_card=evidences_card,
+        )
+
+    # Mostrar los factores de la misma
+    def show_values(self, cpd):
+        print(cpd.values)
+
+    # Adición de las CPDs al modelo
+    def asign_to_model(self, values):
+        for value in values:
+            self.model.add_cpds(value)
+
+    # Verificación del modelo
+    def check_model(self):
+        return self.model.check_model()
+
+    # Verificación de los nodos
+    def check_nodes(self):
+        print("Nodes\n", self.model.nodes())
+
+    # Verificación de los edges
+    def check_edges(self):
+        print("Edges\n", self.model.edges())
+
+    # mostrar la construccion que tiene
+    def show_model_construction(self, value):
+        print("Model\n", self.model.get_cpds(value))        
+
+    # Calcular probabilidad dado elemento
+    def calculate_probability(self, variables, evidenced=None):
+        self.infer = VariableElimination(self.model)
+        query = self.infer.query(variables=variables, evidence=evidenced)
+        return query
--- cool-bayesian-networks/main.py
+++ cool-bayesian-networks/main.py
@@ -1,18 +1,18 @@
-from bayesianConstruct import *
+from bayesian_construct import *
 
 bayesian = Bayesian()
 
-bayesian.BayesionNetworkConstruct([('A', 'C'), ('B', 'C')])
+bayesian.bayesian_network_construction([('A', 'C'), ('B', 'C')])
 
-cpd_a = bayesian.AsignValues('A',2,[[0.3], [0.7]])
-cpd_b = bayesian.AsignValues('B',2,[[0.23], [0.77]])
-cpd_c = bayesian.AsignValues('C',2,[[0.20, 0.77, 0.10, 0.5],[0.80, 0.23, 0.90, 0.5]],['A', 'B'],[2, 2])
+cpd_a = bayesian.asign_values('A', 2, [[0.3], [0.7]])
+cpd_b = bayesian.asign_values('B', 2, [[0.23], [0.77]])
+cpd_c = bayesian.asign_values('C', 2, [[0.20, 0.77, 0.10, 0.5], [0.80, 0.23, 0.90, 0.5]], ['A', 'B'], [2, 2])
 
-bayesian.showValues(cpd_c)
-bayesian.AsignToModel([cpd_a,cpd_b,cpd_c])
-bayesian.checkModel()
-bayesian.checkEdges()
-bayesian.checkNodes()
-bayesian.showModelConstruct('C')
-result = bayesian.calculateProbability(['C'],{'A': 0, 'B': 1})
+bayesian.show_values(cpd_c)
+bayesian.asign_to_model([cpd_a, cpd_b, cpd_c])
+print(bayesian.check_model())
+bayesian.check_edges()
+bayesian.check_nodes()
+bayesian.show_model_construction('C')
+result = bayesian.calculate_probability(['C'], { 'A': 0, 'B': 1 })
 print(result)
